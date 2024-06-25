56d9a31dfe5c12c364341935f5021cd0824a028a
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-06-25 15:14:59 +0100

    Add cipher

---
+++ cipher.rb
@@ -0,0 +1,30 @@
+puts "String: "
+original_string = gets.chomp
+
+puts "Shift factor: "
+shift_factor = gets.chomp
+
+
+
+
+
+def shift_string(string, shift_num)
+    string_array = string.downcase.split("")
+
+   string_ascii = string_array.map do |letter|
+        if letter.ord == 32
+            letter = 32
+        elsif letter.ord.between?(33, 64)
+            letter = letter.ord
+        elsif letter.ord + shift_num.to_i > 122
+            wrap_length = letter.ord + shift_num.to_i - 122
+            letter = 97 + (wrap_length - 1)
+        else
+            letter.ord + shift_num.to_i
+        end
+    end
+    string_ascii.map { |number| number.chr}.join("")
+end
+
+
+p shift_string(original_string, shift_factor)
